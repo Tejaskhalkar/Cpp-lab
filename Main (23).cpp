86b04621a3e895e3d8ce3e1de2f7c98f3c40fcc2
Tejaskhalkar <tejas.khalkar24@sanjivani.edu.in>
2024-10-09 08:58:13 +0530

    Add files via upload

---
+++ Main (23).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// base class vehical
+class vehical{
+  public:
+  void vehical
+  cout<<"I am a vehical"<<endl;
+  
+  
+}
+};
+
+// derived class four-wheelr
+class four-wheelr
+public:
+void four-wheelr
+cout<<"I have fourwheel"<<endl;
+}
+};
+
+//dderived class char
+class car
+public:
+void car
+cout<<"I am a car"<<endl;
+}
+};
+
+int main({
+my car;
+
+my car. car();
+my car. four-wheelr();
+my car. vehical ()
+
+return 0;
+}
+
+
